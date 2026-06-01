a473668d9ba581a65c5f850db26213f146258878
Guido Ozuna <guido-.com@outlook.com>
2026-06-01 07:09:15 -0300

    Merge remoto con local

--- api/index.js
+++ api/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const mapa = require("./mapa.json");
 
+// Endpoint para devolver el mapa
 app.get("/api/mapa", (req, res) => {
   res.json(mapa);
 });
 
+// Exportar la app para que Vercel la use
 module.exports = app;
