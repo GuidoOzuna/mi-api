ea65b4ebc8cd90f4cebd7a3f9ce332a708ec6ee0
Guido Ozuna <guido-.com@outlook.com>
2026-08-27 10:57:53 -0300

    mensaje

---
+++ api/game.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+
+app.get("/api/game", (req, res) => {
+  const mapa = [
+    [1,1,1,1,1],
+    [1,1,1,2,1],
+    [1,1,1,2,1],
+    [1,2,1,1,1],
+    [1,1,1,1,1]
+  ];
+  res.json({ mapa });
+});
+
+export default app;
